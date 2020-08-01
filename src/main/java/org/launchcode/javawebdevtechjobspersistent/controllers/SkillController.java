cff7c296a4d6f9e476740ddd67ea98caa3977b48
webby12333 <webby12333@yahoo.com>
2020-08-01 11:58:04 -0500

    More Test

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -3,16 +3,16 @@ package org.launchcode.javawebdevtechjobspersistent.controllers;
 import org.launchcode.javawebdevtechjobspersistent.models.Skill;
 import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Optional;
 
+@Controller
+@RequestMapping("skills")
 public class SkillController {
 
     @Autowired
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,8 +1,7 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
